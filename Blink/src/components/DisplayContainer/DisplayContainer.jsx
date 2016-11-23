3bcd206312615602f086867a87ae25f4308de17f
Skylar Bowen <bowenskylar>
2016-11-23 10:16:20 -0500

    update schema, created mock form and display list for search results to work off

--- Blink/src/components/DisplayContainer/DisplayContainer.jsx
+++ Blink/src/components/DisplayContainer/DisplayContainer.jsx
@@ -4,6 +4,9 @@ import './DisplayContainer.css';
 class DisplayContainer extends Component {
   render() {
     return (
+      <div className ="display-container"> Search Results Display Results
+      {/* Listed result items will display here*/}
+      </div>
 
     );
   }
