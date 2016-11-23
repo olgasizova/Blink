d817d529f5c7d3f52b0d1ea8568a80b46a6961ad
Skylar Bowen <bowenskylar>
2016-11-23 10:32:27 -0500

    merge conflict complete

--- Blink/src/components/Search/Search.jsx
+++ Blink/src/components/Search/Search.jsx
@@ -4,10 +4,8 @@ import './Search.css';
 class Search extends Component {
   render() {
     return (
-<<<<<<< HEAD
 /* Search Google API for activities to add to
 form will search google places API and save search parameters to later dispay these results */
-=======
        <div id='form-container'>
         <input id="searchForm"
           type="text"
@@ -15,7 +13,6 @@ form will search google places API and save search parameters to later dispay th
           />
         <button className="searchButton">Search</button>
         </div>
->>>>>>> 6fd78c635e3f12500241b0f5d4c39316e60fd5fc
     );
   }
 }
