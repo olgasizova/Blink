9bc499e625118f02138ad58068729326f0333cc3
Olga Sizova <olisizova@gmail.com>
2016-11-25 15:32:30 -0500

    added getAllUsers function

--- models/userData.js
+++ models/userData.js
@@ -1,7 +1,7 @@
 const db = require('./../lib/dbConnect');
 
 function getUserProfile(req, res, next) {
-  db.one(`SELECT id, bday, profile_img FROM user_profiles WHERE session_id = $1;`, [req.sessionID])
+  db.one(`SELECT * FROM users WHERE session_id = $1;`, [req.sessionID])
   .then((data) => {
     res.data = {
       userProfile: data
@@ -19,6 +19,29 @@ function getUserProfile(req, res, next) {
     next();
   })
 }
+
+function getAllUsers(req, res, next) {
+  db.select(`SELECT * FROM users`)
+  .then((data) => {
+    res.data = {
+      userProfile: data
+    }
+    next()
+  })
+  .catch((err) => {
+    res.data = {
+      userProfile: {
+        id: 'No user signed in',
+        bday: undefined
+      }
+    }
+    console.log(`---> Error getUserProfile: ${err}`);
+    next();
+  })
+}
+
+
 module.exports = {
-  getUserProfile
+  getUserProfile,
+  getAllUsers
 }
