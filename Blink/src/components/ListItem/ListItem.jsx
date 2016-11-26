93fad130c632e0d604e89550133df99378581383
Olga Sizova <olisizova@gmail.com>
2016-11-26 18:34:28 -0500

    List Item is now displaying

---
+++ Blink/src/components/ListItem/ListItem.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ListItem.css';
+
+class ListItem extends Component {
+  render() {
+    return (
+<div><h1>List Item</h1></div>
+    );
+  }
+}
+
+export default ListItem;
