3299e4337e78dfc8a4462a3e2a9d62c6faffc658
Skylar Bowen <bowenskylar>
2016-11-26 16:39:34 -0500

    blahhh

--- models/usersBucket.js
+++ models/usersBucket.js
@@ -1,6 +1,8 @@
 const db = require('./../lib/dbConnect');
 
 function getAllActivities(req, res, next) {
+  /*select all from events table where current
+  session id matches session id associated with events */
   db.any('SELECT * from events WHERE session_id = $1')
     .then((activities) => {
       res.activities = activities;
@@ -10,12 +12,15 @@ function getAllActivities(req, res, next) {
 }
 
 function addActivity(req, res, next) {
+  /*insert new activity to events table
+  name formatted address and link with current google session id */
   db.none(`INSERT INTO events (name, formatted_address) VALUES ($1, $2)`, [req.body.name, req.body.formatted_address])
     .then(next())
     .catch(err => next(err));
 }
 
 function deleteEvent(req, res, next) {
+  // delete activity from event where session id attatched to event id lives
   db.none(`DELETE FROM events WHERE id = $1;`, [req.params.session_id])
     .then(next())
     .catch(err => next(err));
@@ -23,4 +28,7 @@ function deleteEvent(req, res, next) {
 
 module.exports={
   //export models here
+  getAllActivities,
+  addActivity,
+  delete,
 }
