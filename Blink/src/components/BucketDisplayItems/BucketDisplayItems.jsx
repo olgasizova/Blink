96e4852e14a7cde2b3de8a876d8756f940a91cd5
Skylar Bowen <bowenskylar>
2016-11-28 15:57:13 -0500

    completed bucket items

--- Blink/src/components/BucketDisplayItems/BucketDisplayItems.jsx
+++ Blink/src/components/BucketDisplayItems/BucketDisplayItems.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ListItem from './../ListItem/ListItem';
 import './BucketDisplayItems.css';
 
+const generateCompleted =(props) =>
+  props.bucket.completed.map((event,i) =>
+    <div key={`CListItem${event.name}${i}`} className="flex-items">
+      <ListItem
+        Title={event.name}
+        address={event.formatted_address}
+        rating={event.rating}
+        image={event.place_img}
+        />
+        <button> delete </button>
+      </div>
+)
+
 const generateSaved =(props) =>
   props.bucket.pending.map((event,i) =>
     <div key={`ListItem${event.name}${i}`} className="flex-items">
@@ -17,8 +30,15 @@ const generateSaved =(props) =>
 
 const BucketDisplayItems = props => (
       <div className="flex-container">
-            {generateSaved(props)}
+        <div className="pendingContainer">
+              {generateSaved(props)}
+        </div>
+        <div className="completedContainer">
+              {generateCompleted(props)}
+        </div>
+
       </div>
+
 );
 
 export default BucketDisplayItems;
