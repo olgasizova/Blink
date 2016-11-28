a708957e957db88124b16060ebe300618250a0a4
Olga Sizova <olisizova@gmail.com>
2016-11-28 17:09:32 -0500

    udpated css

--- Blink/src/components/ListItem/ListItem.jsx
+++ Blink/src/components/ListItem/ListItem.jsx
@@ -5,7 +5,11 @@ class ListItem extends Component {
   render() {
     return (
 
-<div className="list-item"><span className="title">{this.props.Title}</span>
+<div className="list-item">
+ <style>
+    @import url('https://fonts.googleapis.com/css?family=Baloo+Bhaina|Open+Sans');
+  </style>
+<span className="title">{this.props.Title}</span>
     <p className="address">{this.props.address}</p>
     <p className="rating">rating  {this.props.rating}</p>
     <img src={this.props.image} alt={this.props.Title}/>
