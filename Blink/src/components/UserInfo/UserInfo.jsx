82c90ff41fc3ccb8ced93cf786e38596e3156f96
Olga Sizova <olisizova@gmail.com>
2016-11-28 10:52:39 -0500

    updated css for module

--- Blink/src/components/UserInfo/UserInfo.jsx
+++ Blink/src/components/UserInfo/UserInfo.jsx
@@ -7,11 +7,11 @@ class UserInfo extends Component {
       return (
         <div className="user-input-container">
 
-          <input type="number" placeholder="Enter Age" value={this.props.userAge} onChange={this.props.updateFormAge}/>
-          <input type="text" placeholder="Your Gender" value={this.props.userGender} onChange={this.props.updateFormGender}/>
-          <input type="date" value={this.props.userDOB} onChange={this.props.updateFormDOB} />
+          <input className="age" type="number" placeholder="Enter Age" value={this.props.userAge} onChange={this.props.updateFormAge}/>
+          <input className="gender" type="text" placeholder="Enter Gender" value={this.props.userGender} onChange={this.props.updateFormGender}/>
+          <input className="dob" type="date" value={this.props.userDOB} onChange={this.props.updateFormDOB} />
 
-          <button
+          <button className="add-info"
             onClick={this.props.handleDOBSubmit}
             > Add My Info </button>
           </div>
